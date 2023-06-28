c9887037d57d0fcaa8b48f141d99c4ca7fa7b810
yushshresthax <yush.shrestha2013@gmail.com>
2023-06-28 17:28:12 +0545

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,84 +1,96 @@
+import { ResponsiveRadar } from '@nivo/radar';
+import { useEffect, useState } from 'preact/hooks';
 
-import { ResponsiveRadar } from '@nivo/radar'
-import '../pages/home.css'
-import api from '../pages/api'
-const MyResponsiveRadar = ({ data /* see data tab */ }) => (
-    <ResponsiveRadar
-        data={data}
-        keys={[ 'chardonay', 'carmenere', 'syrah' ]}
-        indexBy="taste"
+const RadarChart = () => {
+  const [chartData, setChartData] = useState([]);
+  const playerIndices = [1,2]; 
+  const baseUrl = 'https://sportscore1.p.rapidapi.com/players/';
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'af0ff56796msh245c42a6090e163p115a7djsnb7aee9fbcf6b',
+      'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com'
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const playerData = [];
+        for (const index of playerIndices) {
+          const url = `${baseUrl}${index}`;
+          const response = await fetch(url, options);
+          const result = await response.json();
+		  console.log(result);
+		//   const playerName= result.data.name;
+		  
+          const transformedData = result.data.ability.map((item) => ({
+			name: item.name,
+            value: item.value,
+            // full_average: item.full_average,
+          }));
+          playerData.push(...transformedData);
+		  
+        }
+        setChartData(playerData);
+    	// console.log(playerData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div style={{ backgroundColor: 'azure', width: '50vw', height: '70vh', display: 'flex', flexDirection: 'column', alignItems: 'center', border: '3px solid blue' }}>
+      <ResponsiveRadar
+        data={chartData}
+        keys={['value']}
+        indexBy="name"
         valueFormat=">-.2f"
         margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
-        borderColor={{ from: 'color' }}
+        borderColor="black"
         gridLabelOffset={36}
         dotSize={10}
         dotColor={{ theme: 'background' }}
         dotBorderWidth={2}
-        colors={{ scheme: 'nivo' }}
+        colors={{ scheme: 'accent' }}
+		fillOpacity={0.6}
+		borderWidth={2}
         blendMode="multiply"
         motionConfig="wobbly"
         legends={[
-            {
-                anchor: 'top-left',
-                direction: 'column',
-                translateX: -50,
-                translateY: -40,
-                itemWidth: 80,
-                itemHeight: 20,
-                itemTextColor: '#999',
-                symbolSize: 12,
-                symbolShape: 'circle',
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemTextColor: '#000'
-                        }
-                    }
-                ]
-            }
+          {
+            anchor: 'top-left',
+            direction: 'column',
+            translateX: -50,
+            translateY: -40,
+            itemWidth: 80,
+            itemHeight: 20,
+            itemTextColor: '#999',
+            symbolSize: 12,
+            symbolShape: 'circle',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemTextColor: '#000',
+                },
+              },
+            ],
+			// 	data: chartData.map((data) => ({
+			// 	id: data.player,
+			// 	label: data.player,
+			//   })),
+          },
         ]}
-    />
-)
+      />
+    </div>
+  );
+};
+
 export function Home() {
-	api()
-	return (
-	  <div style={{backgroundColor:'azure', width: '50vw',height: '70vh', display:'flex', flexDirection:'column', alignItems:'center', border:'3px solid blue'}}>
-		
-		<MyResponsiveRadar
-		  data={[
-			{
-			  taste: 'fruity',
-			  chardonay: 56,
-			  carmenere: 66,
-			  syrah: 90,
-			},
-			{
-			  taste: 'bitter',
-			  chardonay: 77,
-			  carmenere: 75,
-			  syrah: 82,
-			},
-			{
-			  taste: 'heavy',
-			  chardonay: 40,
-			  carmenere: 73,
-			  syrah: 90,
-			},
-			{
-			  taste: 'strong',
-			  chardonay: 86,
-			  carmenere: 61,
-			  syrah: 103,
-			},
-			{
-			  taste: 'sunny',
-			  chardonay: 33,
-			  carmenere: 29,
-			  syrah: 119,
-			},
-		  ]}
-		/>
-	  </div>
-	);
-  }
+  return <RadarChart />;
+}
--- src/pages/api.jsx
+++ src/pages/api.jsx
@@ -1,20 +1,21 @@
-
-import axios from 'axios'
+const url = 'https://sportscore1.p.rapidapi.com/players/1';
 
 const options = {
-  method: 'GET',
-  url: 'https://api-football-v1.p.rapidapi.com/v3/timezone',
-  headers: {
-    'X-RapidAPI-Key': 'af0ff56796msh245c42a6090e163p115a7djsnb7aee9fbcf6b',
-    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-  }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'af0ff56796msh245c42a6090e163p115a7djsnb7aee9fbcf6b',
+		'X-RapidAPI-Host': 'sportscore1.p.rapidapi.com'
+	}
 };
-const api = async()=> {
+
+const api = async()=>{
 try {
-	const response = await axios.request(options);
-	console.log(response.data);
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result);
 } catch (error) {
 	console.error(error);
 }
 }
+
 export default api
